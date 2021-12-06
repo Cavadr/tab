7b55b5fc2a91cd560f87c513d049bf934f34edd5
Cavadr <javadir@code.edu.az>
2021-12-07 01:39:48 +0400

    tabs example

---
+++ task.js
@@ -0,0 +1,13 @@
+function openRow(evt, cavad) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabrow");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cavad).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
